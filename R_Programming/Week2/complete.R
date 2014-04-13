3882632b4dd8fa2834126caa6bb88498af063034
jhendric98 <jhendric98@gmail.com>
2014-04-13 13:46:37 -0500

    Split into 3 files for each part of the assignment.

--- R_Programming/Week2/complete.R
+++ R_Programming/Week2/complete.R
@@ -1,22 +1,25 @@
 # week 2 programming assignment.
 #   Jim Hendricks
-#   Part 1
+#   Part 2
 
-pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   setwd("~/Documents/kaggle/datasciencecoursera/R_Programming/Week2")
   
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
 }
+
 
 # script to submit code for grading
 source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
---
+++ R_Programming/Week2/corr.R
@@ -0,0 +1,20 @@
+# week 2 programming assignment.
+#   Jim Hendricks
+#   Part 3
+
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  setwd("~/Documents/kaggle/datasciencecoursera/R_Programming/Week2")
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+}
+
+
+# script to submit code for grading
+source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
